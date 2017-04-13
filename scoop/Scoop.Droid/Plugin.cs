163a70aaf1389786b371a7fe3a86758397ffdaf0
Luke Pothier <lukepothier@gmail.com>
2017-04-14 00:28:09 +0200

    Android: WIP showing overlay with WindowManager

--- scoop/Scoop.Droid/Plugin.cs
+++ scoop/Scoop.Droid/Plugin.cs
@@ -1,14 +1,58 @@
 using MvvmCross.Platform;
 using MvvmCross.Platform.Plugins;
+using System.Collections.Generic;
 
 namespace Scoop.Droid
 {
     [Preserve(AllMembers = true)]
-    public class Plugin : IMvxPlugin
+    public class Plugin : IMvxConfigurablePlugin
     {
+        bool _includeDateTime = true;
+        bool _includePlatform = true;
+        bool _includeVersionNumber = true;
+        bool _includeDeviceName = true;
+        bool _includeNetworkState;
+        string _dateTimeFormat = "yyyy-MM-dd HH:mm:ss zzz";
+        int _fontSize = 8;
+        ICollection<string> _customData;
+
+        public void Configure(IMvxPluginConfiguration configuration)
+        {
+            if (configuration == null)
+                return;
+
+            var scoopConfiguration = (ScoopConfiguration)configuration;
+
+            _includeDateTime = scoopConfiguration.IncludeDateTime;
+            _includePlatform = scoopConfiguration.IncludePlatform;
+            _includeVersionNumber = scoopConfiguration.IncludeVersionNumber;
+            _includeDeviceName = scoopConfiguration.IncludeDeviceName;
+            _includeNetworkState = scoopConfiguration.IncludeNetworkState;
+            _dateTimeFormat = scoopConfiguration.DateTimeFormat;
+            _fontSize = scoopConfiguration.FontSize;
+            _customData = scoopConfiguration.CustomData;
+        }
+
         public void Load()
         {
-            Mvx.RegisterSingleton<IScoopOverlay>(new ScoopOverlay());
+            var configuration = new ScoopConfiguration
+            {
+                IncludeDateTime = _includeDateTime,
+                IncludePlatform = _includePlatform,
+                IncludeVersionNumber = _includeVersionNumber,
+                IncludeDeviceName = _includeDeviceName,
+                IncludeNetworkState = _includeNetworkState,
+                DateTimeFormat = _dateTimeFormat,
+                FontSize = _fontSize,
+                CustomData = _customData
+            };
+
+            var instance = new ScoopOverlay
+            {
+                Configuration = configuration
+            };
+
+            Mvx.RegisterSingleton<IScoopOverlay>(instance);
         }
     }
 }
--- scoop/Scoop.Droid/ScoopOverlay.cs
+++ scoop/Scoop.Droid/ScoopOverlay.cs
@@ -1,10 +1,39 @@
+using Android.Graphics;
+using Android.Views;
+using MvvmCross.Platform;
+using MvvmCross.Platform.Droid.Platform;
+using System;
+using static Android.Views.ViewGroup;
+
 namespace Scoop.Droid
 {
     [Preserve(AllMembers = true)]
     public class ScoopOverlay : IScoopOverlay
     {
+        public ScoopConfiguration Configuration { get; set; }
+
         public void ShowOverlay()
         {
+            Mvx.Trace($"{nameof(ShowOverlay)} called");
+
+            var context = Mvx.Resolve<IMvxAndroidCurrentTopActivity>().Activity;
+
+            var overlayView = new Android.Widget.TextView(context)
+            {
+                Text = $"Testing: {DateTime.Now.ToString(Configuration.DateTimeFormat)}"
+            };
+
+            var flags = WindowManagerFlags.Fullscreen | WindowManagerFlags.WatchOutsideTouch | WindowManagerFlags.AllowLockWhileScreenOn | WindowManagerFlags.NotTouchable | WindowManagerFlags.NotFocusable;
+
+            var layoutParams = new WindowManagerLayoutParams(LayoutParams.MatchParent, LayoutParams.MatchParent, WindowManagerTypes.ApplicationPanel, flags, Format.Translucent)
+            {
+                Gravity = GravityFlags.Top | GravityFlags.Right
+            };
+
+            var windowManager = context.WindowManager;
+
+            // TODO :: deal with WindowManagerBadTokenExceptions
+            windowManager.AddView(overlayView, layoutParams);
         }
     }
 }
